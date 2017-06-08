e38ec29881b02f39c4e01ebd6b2fb642e7a4da92
sxu459 <sxu459@aucklanduni.ac.nz>
2017-06-08 18:55:57 +1200

    Create RevenueTest2.cs

--- RevenueTest2.cs
+++ RevenueTest2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +10,7 @@ class RevenueTest2
     public string createdAt { get; set; }
     public string updatedAt { get; set; }
     public string version { get; set; }
-    public string ProductID { get; set; }
     public string Date { get; set; }
-    public string Units { get; set; }
     public string Revenue { get; set; }
     public string City { get; set; }
     public string State { get; set; }
@@ -20,4 +18,8 @@ class RevenueTest2
     public string IsCompete { get; set; }
     public string Segment { get; set; }
     public string deleted { get; set; }
+    public int ProductID { get; set; }
+    public int Units { get; set; }
+    public int Z { get; set; }
 }
+
